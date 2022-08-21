bb20cddabb1c5192eccc622af383ec51dd609d7d
SongYi Han <102973866+songYiZZ@users.noreply.github.com>
2022-08-22 00:16:47 +0900

    [Bronze III] Title: 알람 시계, Time: 228 ms, Memory: 18500 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int H = sc.nextInt();
+        int M = sc.nextInt();
+
+        if(M >= 45 ){
+            System.out.println(H + " " + (M-45));
+        }else{
+            if(H>0){
+                System.out.println((H-1) + " " + (M+15));
+            }else{
+                System.out.println((H+23) + " " + (M+15));
+            }
+
+        }
+    }
+}
