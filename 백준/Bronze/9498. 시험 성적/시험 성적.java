b2cfa153edcdbc2cdf0c0a7af72926bbc7eec9e1
SongYi Han <102973866+songYiZZ@users.noreply.github.com>
2022-08-22 00:16:07 +0900

    [Bronze V] Title: 시험 성적, Time: 208 ms, Memory: 17692 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        System.out.println(A>=90?"A":A>=80?"B":A>=70?"C":A>=60?"D":"F");
+    }
+}
